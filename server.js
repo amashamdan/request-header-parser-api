d2cc881d98c85108ca290dfb43cbfa9f8aa06d48
Amer Hamdan <amashamdan@gmail.com>
2016-10-03 12:16:29 -0700

    Feat: Added language to the response.
    Language is now added to the response JSON.
    Again, us-parser module used.
    API reaady for use.

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.get("/", function(req, res) {
     var parsedUA = parser.parse(req.headers["user-agent"]);
     var os = parsedUA.string;
 	result.os = os.slice(os.indexOf("(") + 1, os.indexOf(")"));
+	// The following three lines extract language from the parsed request header.
+	var parsedLanguage = parser.parse(req.headers["accept-language"]);
+	var language = parsedLanguage.string;
+	result.language = language.slice(0, language.indexOf(","));
     // Send response json.
     res.send(result);
     res.end();
 });
-
+// port 3000 used for localhost during development.
 var port = Number(process.env.PORT || 3000)
 app.listen(port);
